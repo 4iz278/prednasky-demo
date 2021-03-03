464f1ab5c883948914dfcd5818a77a3691391afe
Jirka Kosek <jirka@kosek.cz>
2021-03-03 15:06:58 +0100

    Added examples of header handling

---
+++ http-hlavicky/content-type.php
@@ -0,0 +1,12 @@
+<?php
+echo Header('Content-type: text/plain');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        <h1>Uvidíme zdroják HTML, protože prohlížeč bude dokument parsovat jako text</h1>
+    </body>
+</html>
