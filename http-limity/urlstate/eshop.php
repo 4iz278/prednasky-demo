fd574e15e3d56c678b839b64516c1ebce3af58a1
Jirka Kosek <jirka@kosek.cz>
2021-03-08 13:11:51 +0100

    Preserving state in URL example

---
+++ http-limity/urlstate/eshop.php
@@ -0,0 +1,57 @@
+<?php
+  $zbozi = array("pivo", "chleba", "med");
+  $kosik = array();
+
+  // obnovení stavu košíku z URL
+  foreach($zbozi as $z)
+  {
+    if (is_numeric($_REQUEST[$z]))
+    {
+      $kosik[$z] = $_REQUEST[$z];
+    }
+  }
+
+  // aktualizace košíku, pokud bylo něco přidáno
+  if ($_REQUEST["pridej"])
+  {
+      if (isset($kosik[$_REQUEST["pridej"]]))
+      {
+        $kosik[$_REQUEST["pridej"]]++;
+      }
+      else
+      {
+        $kosik[$_REQUEST["pridej"]] = 1;
+      }
+  }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Eshop roku</title>
+    </head>
+    <body>
+        <h1>Eshop roku</h1>
+
+        <h2>Katalog</h2>
+
+        <ul>
+<?php
+foreach ($zbozi as $z) {
+    echo "<li>" . $z . " <a href='?pridej=$z&" . http_build_query($kosik) . "'>Přidej do košíku</a></li>";
+}
+?>
+        </ul>
+
+        <h2>Košík</h2>
+
+        <ul>
+<?php
+foreach ($kosik as $z => $pocet) {
+    echo "<li>" . $z . " " . $pocet . " ks</li>";
+}
+?>
+        </ul>
+        
+    </body>
+</html>
