66f1856bc8fd33afe375286d0069d83eb4d194f5
Jirka Kosek <jirka@kosek.cz>
2023-03-09 20:09:11 +0100

    Added another example

---
+++ http-limity/eshop-webstorage.php
@@ -0,0 +1,51 @@
+<?php
+  $zbozi = array("pivo", "chleba", "med");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Eshop roku</title>
+        <script>
+            function pridej(zbozi) {
+                var aktualniStav = localStorage.getItem(zbozi);
+                if (aktualniStav != null) {
+                    localStorage.setItem(zbozi, aktualniStav + 1);
+                } else {
+                    localStorage.setItem(zbozi, 1);
+                }
+
+                aktualizujKosik();
+            }
+
+            function aktualizujKosik() {
+                var kosik = document.getElementById("kosik");
+                kosik.innerHTML = "";
+                for (var i=0; i<localStorage.length; i++)
+                {
+                    var key = storage.key(i);
+                    var value = storage.getItem(key);
+                    kosik.innerHTML += "<li>" + key + " - " + value + "</li>";
+                }
+            }
+        </script>
+    </head>
+    <body>
+        <h1>Eshop roku – stav držíme v session</h1>
+
+        <h2>Katalog</h2>
+
+        <ul>
+<?php
+  foreach ($zbozi as $z) {
+    echo "<li>" . $z . " – <a href='javascript:pridej(\"$z\")'>přidej do košíku</a></li>";
+}
+?>
+        </ul>
+
+        <h2>Košík</h2>
+
+        <ul id="kosik">
+        </ul>
+    </body>
+</html>
